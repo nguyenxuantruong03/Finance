79c4127b04b63120da669aea3a6156711be85780
Xuan Truong <nxt159753@gmail>
2024-05-14 21:47:42 +0700

    PATCH and DELETE API

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -9,5 +9,7 @@ const app = new Hono().basePath("/api");
 const routes = app.route("/accounts", accounts);
 export const GET = handle(app);
 export const POST = handle(app);
+export const PATCH = handle(app);
+export const DELETE = handle(app);
 
 export type AppType = typeof routes;
